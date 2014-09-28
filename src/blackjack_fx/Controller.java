47c713945608f52f5f4127ae74bf97be1c247abc
Adam Gerber <adam.gerber@yahoo.com>
2014-09-28 18:48:45 -0500

    created fx base code

---
+++ src/blackjack_fx/Controller.java
@@ -0,0 +1,9 @@
+package blackjack_fx;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void btnGo_action(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
+}
